2dd15670447fcf09012639f1ade9552d5561b8da
Harsh Preet <harshpreets63@gmail.com>
2021-07-30 14:38:14 +0530

    Update and rename havoc_whyred.mk to komodo_whyred.mk

--- komodo_whyred.mk
+++ komodo_whyred.mk
@@ -20,7 +20,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
+$(call inherit-product, vendor/komodo/config/common_full_phone.mk)
 
 # Inherit from custom vendor
 $(call inherit-product, vendor/MiuiCamera/config.mk)
